8783226cae88a61267cca80db4044ef28d82370d
Cory Coward <cory@c2softwaresolutions.com>
2020-05-26 18:55:14 -0500

    Switch to Postgres database

--- time_bank/settings.py
+++ time_bank/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'time_bank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'timebank',
+        'USER': 'timebank',
+        'PASSWORD': 'sC7zV8jQwgG3CU8d',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
