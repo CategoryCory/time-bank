841206566b35e1989fcdc93ac6bf463f6fb1d97c
Cory Coward <cory@c2softwaresolutions.com>
2020-05-26 13:54:00 -0500

    Add fields to custom user, set up custom user admin

--- users/admin.py
+++ users/admin.py
@@ -9,9 +9,28 @@ CustomUser = get_user_model()
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
+    """
+    add_fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('email', )}),
+    )
+
+    add_fieldsets = (
+        (None, {'classes': ('wide', ), 'fields': {'username', 'email', 'password1', 'password2'}}),
+    )
+    """
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', ]
+    list_display = ['email', 'username', 'first_name', 'last_name', 'is_approved', 'is_staff', ]
+    list_editable = ['is_approved', ]
+    fieldsets = (
+        ('Account Information', {'fields': ('username', 'email', 'first_name', 'last_name',)}),
+        ('Personal Information', {'fields': ('date_of_birth', 'biography',)}),
+        ('Address', {'fields': ('address_street', 'address_city', 'address_state', 'address_zip',)}),
+        ('Social Media', {'fields': ('social_facebook', 'social_twitter', 'social_instagram', 'social_linkedin',)}),
+        ('Permissions',
+         {'fields': ('is_active', 'is_approved', 'is_staff', 'is_superuser', 'groups', 'user_permissions',)}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined',)}),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(default=timezone.now)
+    is_approved = models.BooleanField(verbose_name='Approved',
+                                      help_text='Designates whether this user has been approved to post on this site.',
+                                      default=False)
+    address_street = models.CharField(verbose_name='Street Address', max_length=200)
+    address_city = models.CharField(verbose_name='City', max_length=75)
+    address_state = models.CharField(verbose_name='State', max_length=50)
+    address_zip = models.CharField(verbose_name='ZIP Code', max_length=50)
+    biography = models.TextField()
+    social_facebook = models.URLField(verbose_name='Facebook', max_length=200, blank=True)
+    social_twitter = models.URLField(verbose_name='Twitter', max_length=200, blank=True)
+    social_instagram = models.URLField(verbose_name='Instagram', max_length=200, blank=True)
+    social_linkedin = models.URLField(verbose_name='LinkedIn', max_length=200, blank=True)
 
     def __str__(self):
         return self.email
