e33a58c32ee4ef3d9d2f6a53fbf8eb26708b77a5
Cory Coward <cory@c2softwaresolutions.com>
2020-05-31 22:11:51 -0500

    Add user detail url, view, and template

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.list import ListView
+from django.views.generic import DetailView, ListView
 from django.contrib.auth import get_user_model
 
 CustomUser = get_user_model()
@@ -7,3 +7,9 @@ CustomUser = get_user_model()
 class UserListView(ListView):
     model = CustomUser
     paginate_by = 25
+
+
+class UserDetailView(DetailView):
+    model = CustomUser
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
