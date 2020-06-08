bfaac010e2ff5654c2779aa5a81284e9b2b8f18e
Cory Coward <cory@c2softwaresolutions.com>
2020-06-08 15:12:25 -0500

    Add Requirement model to pages app

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Requirement
 
-# Register your models here.
+
+class RequirementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'is_active', )
+    list_per_page = 25
+    list_editable = ('is_active', )
+    list_filter = ('is_active', )
+
+
+admin.site.register(Requirement, RequirementAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Requirement(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField()
+    body = models.TextField()
+    order = models.IntegerField(default=1)
+    is_active = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
