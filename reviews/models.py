355fd6a9204528e48edf798f20cdff81e19aea45
Cory Coward <cory@c2softwaresolutions.com>
2021-03-11 08:46:55 -0600

    Remove import from reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-CustomUser = get_user_model()
-
 
 class UserReview(models.Model):
     rating = models.FloatField(default=0.0)
     comments = models.CharField(max_length=200, blank=True)
-    reviewee = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reviewee')
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='author')
+    reviewee = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='reviewee')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='author')
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
