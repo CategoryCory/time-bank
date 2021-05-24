767b7b6b7b7d3a4d5c622c2d6cbb713b49fce466
Cory Coward <cory@c2softwaresolutions.com>
2021-05-24 16:25:25 -0500

    Add about page

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'pages'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
     path('terms-of-service/', views.TermsView.as_view(), name='terms'),
     path('privacy-policy/', views.PrivacyPolicyView.as_view(), name='privacy-policy'),
     # path('browse-users/', BrowseUsersView.as_view(), name='browse_users'),
--- pages/views.py
+++ pages/views.py
@@ -8,6 +8,10 @@ class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
 
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+
 class BrowseUsersView(ListView):
     model = CustomUser
     template_name = 'pages/browse_users.html'
